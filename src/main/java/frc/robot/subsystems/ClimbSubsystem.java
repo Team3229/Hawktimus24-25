1e826f3a0c5a2693ad4faff8f4f3f22945e6e36b
3229Software <asherbenincosa@gmail.com>
2025-02-05 15:26:27 -0800

    Modified ClimbSubsystem code as per the review.

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -7,6 +7,7 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,13 +15,8 @@ public class ClimbSubsystem extends SubsystemBase {
     private SparkMax climbMotor;
     private SparkMaxConfig motorConfig;
 
-    private static final double MAX_SPEED = 1;
     private static final int CAN_ID = 10;
-    private static final double POSITION_CONVERSION_FACTOR = -1; //Change this value to the correct conversion factor later.
-
-    private static final double kP = 0.1; //Change these values to the correct PID values later.
-    private static final double kI = 0.0; //Change these values to the correct PID values later.
-    private static final double kD = 0.0; //Change these values to the correct PID values later.
+    private static final double POSITION_CONVERSION_FACTOR = 1; //Change this value to the correct conversion factor later.
     
     public ClimbSubsystem(DoubleSupplier joystickInput) {
         climbMotor = new SparkMax(CAN_ID, MotorType.kBrushless);
@@ -34,21 +30,14 @@ public class ClimbSubsystem extends SubsystemBase {
         motorConfig.softLimit
             .forwardSoftLimitEnabled(true)
 
-            /*Change the value of -1 to the correct forward soft limit later,
+            /*Change the value of 1 to the correct forward soft limit later,
             and double check that POSITION_CONVERSION_FACTOR is used correctly. */
-            .forwardSoftLimit(-1 * POSITION_CONVERSION_FACTOR)
+            .forwardSoftLimit(1 * POSITION_CONVERSION_FACTOR)
             .reverseSoftLimitEnabled(true)
 
             /*Change the value of -1 to the correct forward soft limit later,
             and double check that POSITION_CONVERSION_FACTOR is used correctly. */
-            .reverseSoftLimit(-1 * POSITION_CONVERSION_FACTOR);
-
-        motorConfig.closedLoop
-            .pid(kP, kI, kD)
-            .outputRange(
-                -MAX_SPEED,
-                MAX_SPEED
-            );
+            .reverseSoftLimit(1 * POSITION_CONVERSION_FACTOR);
 
         setDefaultCommand(
             Commands.run(
@@ -56,5 +45,10 @@ public class ClimbSubsystem extends SubsystemBase {
                     climbMotor.set(joystickInput.getAsDouble());
                 }, this)
         );
+    }
+    
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.addDoubleProperty("Climb Position", () -> climbMotor.getEncoder().getPosition(), null);
     }
 }  
