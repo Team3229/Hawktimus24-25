9924674b3ece45a4568743dc34429acccccae088
Nathan <98428552+Xolverr@users.noreply.github.com>
2025-02-10 11:04:32 -0500

    switch to extend swerve

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -11,67 +11,63 @@ import frc.hawklibraries.drivetrains.swerve.SwerveModuleConfig;
 import frc.hawklibraries.drivetrains.swerve.SwerveModuleConfig.SwerveModuleConfigBuilder;
 import frc.hawklibraries.vendorRewrites.wpilib.ChassisSpeeds;
 
-public class DriveSubsystem {
+public class DriveSubsystem extends SwerveDrivetrain {
 
-    public SwerveDrivetrain drivetrain;
-    private SwerveDrivetrainConfig config;
+    private static SwerveDrivetrainConfig config = SwerveDrivetrainConfig.builder()
+        .robotWidth(Inch.of(27))
+        .robotLength(Inch.of(27))
+        .maxAccel(MetersPerSecondPerSecond.of(1.0))
+        .maxSpeed(MetersPerSecond.of(5.24))
+        .maxTurnRate(RadiansPerSecond.of(Math.PI))
+        .moduleDistance(Inch.of(26))
+        .frontLeftConfig(
+            baseModuleConfig()
+            .driveID(1)
+            .turningID(2)
+            .invertDriveMotor(false)
+            .encoderOffset(null)
+            .build()
+        )
+        .frontRightConfig(
+            baseModuleConfig()
+            .driveID(3)
+            .turningID(4)
+            .invertDriveMotor(false)
+            .encoderOffset(null)
+            .build()
+        )
+        .backLeftConfig(
+            baseModuleConfig()
+            .driveID(5)
+            .turningID(6)
+            .invertDriveMotor(false)
+            .encoderOffset(null)
+            .build()
+        )
+        .backRightConfig(
+            baseModuleConfig()
+            .driveID(7)
+            .turningID(8)
+            .invertDriveMotor(false)
+            .encoderOffset(null)
+            .build()
+        )
+        .build();
 
-    private SwerveModuleConfigBuilder<?,?> baseModuleConfig() {
+    private static SwerveModuleConfigBuilder<?,?> baseModuleConfig() {
 
         return SwerveModuleConfig.builder()
             .driveGearRatio(5.9)
             .maxSpeed(MetersPerSecond.of(5.24))
             .wheelDiameter(Inch.of(4))
             .turningGearRatio(12.8)
-            .turningOutputMax(-1)
-            .turningOutputMin(1);
+            .turningOutputMin(-1)
+            .turningOutputMax(1);
 
     }
 
     public DriveSubsystem() {
-
-        config = SwerveDrivetrainConfig.builder()
-            .robotWidth(Inch.of(27))
-            .robotLength(Inch.of(27))
-            .maxAccel(MetersPerSecondPerSecond.of(1.0))
-            .maxSpeed(MetersPerSecond.of(5.24))
-            .maxTurnRate(RadiansPerSecond.of(Math.PI))
-            .moduleDistance(Inch.of(26))
-            .frontLeftConfig(
-                baseModuleConfig()
-                .driveID(1)
-                .turningID(2)
-                .invertDriveMotor(false)
-                .encoderOffset(null)
-                .build()
-            )
-            .frontRightConfig(
-                baseModuleConfig()
-                .driveID(3)
-                .turningID(4)
-                .invertDriveMotor(false)
-                .encoderOffset(null)
-                .build()
-            )
-            .backLeftConfig(
-                baseModuleConfig()
-                .driveID(5)
-                .turningID(6)
-                .invertDriveMotor(false)
-                .encoderOffset(null)
-                .build()
-            )
-            .backRightConfig(
-                baseModuleConfig()
-                .driveID(7)
-                .turningID(8)
-                .invertDriveMotor(false)
-                .encoderOffset(null)
-                .build()
-            )
-            .build();
-
-        drivetrain = new SwerveDrivetrain(
+        super(
             config,
             () -> {
                 return new ChassisSpeeds();
