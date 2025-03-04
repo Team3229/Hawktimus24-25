eb924fb2927a27c68ba268131d8c0626bbf1ee98
3229 Software <98428552+Xolverr@users.noreply.github.com>
2025-03-04 15:13:17 -0500

    Fixed an issue that was messing up the reef zones (i so didn't make this issue)

--- src/main/java/frc/robot/constants/ReefPositions.java
+++ src/main/java/frc/robot/constants/ReefPositions.java
@@ -9,18 +9,18 @@ import lombok.Getter;
 @Getter
 public enum ReefPositions {
     
-    A(new Pose2d(3.190, 3.860, Rotation2d.fromDegrees(180))),
-    B(new Pose2d(3.190, 3.557, Rotation2d.fromDegrees(180))),
-    C(new Pose2d(3.981, 2.821, Rotation2d.fromDegrees(240))),
-    D(new Pose2d(4.210, 2.680, Rotation2d.fromDegrees(240))),
-    E(new Pose2d(5.280, 2.990, Rotation2d.fromDegrees(300))),
-    F(new Pose2d(5.458, 3.092, Rotation2d.fromDegrees(300))),
-    G(new Pose2d(5.458, 4.152, Rotation2d.fromDegrees(000))),
-    H(new Pose2d(5.790, 4.492, Rotation2d.fromDegrees(000))),
-    I(new Pose2d(4.985, 5.235, Rotation2d.fromDegrees(060))),
-    J(new Pose2d(4.740, 5.376, Rotation2d.fromDegrees(060))),
-    K(new Pose2d(3.684, 5.059, Rotation2d.fromDegrees(120))),
-    L(new Pose2d(3.466, 4.913, Rotation2d.fromDegrees(120))),
+    A(new Pose2d(3.190, 4.180, Rotation2d.fromDegrees(180))),
+    B(new Pose2d(3.190, 3.860, Rotation2d.fromDegrees(180))),
+    C(new Pose2d(3.685, 2.981, Rotation2d.fromDegrees(240))),
+    D(new Pose2d(3.970, 2.810, Rotation2d.fromDegrees(240))),
+    E(new Pose2d(4.990, 2.800, Rotation2d.fromDegrees(300))),
+    F(new Pose2d(5.280, 2.970, Rotation2d.fromDegrees(300))),
+    G(new Pose2d(5.800, 3.850, Rotation2d.fromDegrees(000))),
+    H(new Pose2d(5.800, 4.180, Rotation2d.fromDegrees(000))),
+    I(new Pose2d(5.280, 5.080, Rotation2d.fromDegrees(060))),
+    J(new Pose2d(5.000, 5.240, Rotation2d.fromDegrees(060))),
+    K(new Pose2d(3.990, 5.250, Rotation2d.fromDegrees(120))),
+    L(new Pose2d(3.690, 5.080, Rotation2d.fromDegrees(120))),
     Center(new Pose2d(4.489337, 4.025923, Rotation2d.fromDegrees(0)));
 
     Pose2d position;
