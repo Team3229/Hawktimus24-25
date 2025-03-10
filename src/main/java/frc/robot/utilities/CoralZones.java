be660a4b925c2172401ca297209260989386685a
3229 Software <98428552+Xolverr@users.noreply.github.com>
2025-03-10 16:43:12 -0400

    Updated coral zones

--- src/main/java/frc/robot/utilities/CoralZones.java
+++ src/main/java/frc/robot/utilities/CoralZones.java
@@ -1,100 +1,114 @@
 package frc.robot.utilities;
 
+import org.ironmaple.utils.FieldMirroringUtils;
+
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.hawklibraries.utilities.Alliance;
+import frc.hawklibraries.utilities.Alliance.AllianceColor;
+import frc.robot.constants.ReefPositions;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class CoralZones {
+
+	private static Pose2d reefCenterPose = ReefPositions.Center.getPosition();
     
     private static double al(double x) {
-		return -(1 / Math.sqrt(3)) * (x - 3.673422) + 4.496925;
+		return -(1 / Math.sqrt(3)) * (x - reefCenterPose.getX()) + reefCenterPose.getY();
 	}
 
 	private static double bc(double x) {
-		return (1 / Math.sqrt(3)) * (x - 3.673480) + 3.554821;
+		return (1 / Math.sqrt(3)) * (x - reefCenterPose.getX()) + reefCenterPose.getY();
 	}
 
 	private static double de() {
-		return 4.489395;
+		return reefCenterPose.getX();
 	}
 
 	public static Command findCoralZone(
-            boolean rightSide,
+            boolean leftSide,
             DriveSubsystem driveSubsystem
         ) {
 
-		Command out = Commands.none();
+		reefCenterPose = FieldMirroringUtils.toCurrentAlliancePose(ReefPositions.Center.getPosition());
 
         System.out.println("Finding Coral Zone");
 
 		//LEFT SIDE
-		if (rightSide && inZone_AL_BC(driveSubsystem.getPose())) {
+		if (leftSide && inZone_AL_BC(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone AL_BC");
-			out = driveToReef("A");
+			return driveToReef("A");
 		}
-		if (rightSide && inZone_BC_DE(driveSubsystem.getPose())) {
+		if (leftSide && inZone_BC_DE(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone BC_DE");
-			out = driveToReef("C");
+			return driveToReef("C");
 		}
-		if (rightSide && inZone_DE_FG(driveSubsystem.getPose())) {
+		if (leftSide && inZone_DE_FG(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone DE_FG");
-			out = driveToReef("F");
+			return driveToReef("F");
 		}
-		if (rightSide && inZone_FG_HI(driveSubsystem.getPose())) {
+		if (leftSide && inZone_FG_HI(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone FG_HI");
-			out = driveToReef("H");
+			return driveToReef("H");
 		}
-		if (rightSide && inZone_HI_JK(driveSubsystem.getPose())) {
+		if (leftSide && inZone_HI_JK(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone HI_JK");
-			out = driveToReef("J");
+			return driveToReef("J");
 		}
-		if (rightSide && inZone_JK_AL(driveSubsystem.getPose())) {
+		if (leftSide && inZone_JK_AL(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone JK_AL");
-			out = driveToReef("K");
+			return driveToReef("K");
 		}
 		
 
 		//RIGHT SIDE
-		if (!rightSide && inZone_AL_BC(driveSubsystem.getPose())) {
+		if (!leftSide && inZone_AL_BC(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone AL_BC");
-			out = driveToReef("B");
+			return driveToReef("B");
 		}
-		if (!rightSide && inZone_BC_DE(driveSubsystem.getPose())) {
+		if (!leftSide && inZone_BC_DE(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone BC_DE");
-			out = driveToReef("D");
+			return driveToReef("D");
 		}
-		if (!rightSide && inZone_DE_FG(driveSubsystem.getPose())) {
+		if (!leftSide && inZone_DE_FG(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone DE_FG");
-			out = driveToReef("E");
+			return driveToReef("E");
 		}
-		if (!rightSide && inZone_FG_HI(driveSubsystem.getPose())) {
+		if (!leftSide && inZone_FG_HI(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone FG_HI");
-			out = driveToReef("G");
+			return driveToReef("G");
 		}
-		if (!rightSide && inZone_HI_JK(driveSubsystem.getPose())) {
+		if (!leftSide && inZone_HI_JK(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone HI_JK");
-			out = driveToReef("I");
+			return driveToReef("I");
 		}
-		if (!rightSide && inZone_JK_AL(driveSubsystem.getPose())) {
+		if (!leftSide && inZone_JK_AL(driveSubsystem.getPose(), Alliance.getAlliance() == AllianceColor.Blue)) {
             System.out.println("Robot is in zone JK_AL");
-			out = driveToReef("L");
+			return driveToReef("L");
 		}
 
-		out.addRequirements(driveSubsystem);
-
-		return out;
+		System.out.println("Robot is not positioned inside a zone");
+		return Commands.none();
 
 	}
 
     private static Command driveToReef(String reef) {
         try {
+
+            PathConstraints constraints = new PathConstraints(
+			    4, 4.0,
+			    4, Units.degreesToRadians(720)
+		    );
+
             return AutoBuilder.pathfindThenFollowPath(
                 PathPlannerPath.fromPathFile(reef),
-                PathFindingConstraints.SEMIAUTO_ROBOT_CONSTRAINTS
+                constraints
             );
 
         } catch (Exception e) {
@@ -103,41 +117,72 @@ public class CoralZones {
         }
     }
 
-	private static boolean inZone_AL_BC(Pose2d robotPose) {
+	
+
+	private static boolean inZone_AL_BC(Pose2d robotPose, boolean blue) {
+		if(blue){
+			return 
+				(robotPose.getY() < al(robotPose.getX())) && 
+				(robotPose.getY() > bc(robotPose.getX()));
+		} 
+		return 
+			(robotPose.getY() > al(robotPose.getX())) && 
+			(robotPose.getY() < bc(robotPose.getX()));
+	}
+
+	private static boolean inZone_BC_DE(Pose2d robotPose, boolean blue) {
+		if(blue){
+			return 
+				(robotPose.getY() < bc(robotPose.getX())) && 
+				(robotPose.getX() < de());
+		}
+		return
+			(robotPose.getY() > bc(robotPose.getX())) && 
+			(robotPose.getX() > de());
+	}
+
+	private static boolean inZone_DE_FG(Pose2d robotPose, boolean blue) {
+		if(blue){
+			return 
+				(robotPose.getY() < al(robotPose.getX())) && 
+				(robotPose.getX() > de());
+		} 
+		return 
+			(robotPose.getY() > al(robotPose.getX())) && 
+			(robotPose.getX() < de());
+	}
+
+	private static boolean inZone_FG_HI(Pose2d robotPose, boolean blue) {
+		if(blue){
+			return 
+				(robotPose.getY() > al(robotPose.getX())) && 
+				(robotPose.getY() < bc(robotPose.getX()));
+		} 
 		return 
 			(robotPose.getY() < al(robotPose.getX())) && 
 			(robotPose.getY() > bc(robotPose.getX()));
 	}
 
-	private static boolean inZone_BC_DE(Pose2d robotPose) {
+	private static boolean inZone_HI_JK(Pose2d robotPose, boolean blue) {
+		if(blue){
+			return 
+				(robotPose.getY() > bc(robotPose.getX())) && 
+				(robotPose.getX() > de());
+		} 
 		return 
-			(robotPose.getY() < bc(robotPose.getX())) && 
+			(robotPose.getY() < al(robotPose.getX())) && 
 			(robotPose.getX() < de());
 	}
 
-	private static boolean inZone_DE_FG(Pose2d robotPose) {
-		return
+	private static boolean inZone_JK_AL(Pose2d robotPose, boolean blue) {
+		if(blue){
+			return 
+				(robotPose.getY() > al(robotPose.getX())) && 
+				(robotPose.getX() < de());
+		} 
+		return 
 			(robotPose.getY() < al(robotPose.getX())) && 
 			(robotPose.getX() > de());
-	}
-
-	private static boolean inZone_FG_HI(Pose2d robotPose) {
-		return
-            (robotPose.getY() > al(robotPose.getX())) && 
-            (robotPose.getY() < bc(robotPose.getX()));
-			
-	}
-
-	private static boolean inZone_HI_JK(Pose2d robotPose) {
-		return 
-			(robotPose.getY() > bc(robotPose.getX())) && 
-			(robotPose.getX() > de());
-	}
-
-	private static boolean inZone_JK_AL(Pose2d robotPose) {
-		return
-			(robotPose.getY() > al(robotPose.getX())) && 
-			(robotPose.getX() < de());
 	}
 
 }
