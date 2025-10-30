83dcb0d659ffecf074224a8fd14defb219f546ae
3229Software <owenzockoll@icloud.com>
2025-10-30 15:57:19 -0400

    i meant .3 i set it to .35 (ignore the previous .8) i was tired ok

--- src/main/java/frc/robot/subsystems/coral/SpitterSubsystem.java
+++ src/main/java/frc/robot/subsystems/coral/SpitterSubsystem.java
@@ -26,7 +26,7 @@ public class SpitterSubsystem extends SubsystemBase {
     private static final int SENSOR_PORT = 8;
 
     private static final double INTAKE_SPEED = 0.3;
-    private static final double SPIT_SPEED = 0.8;
+    private static final double SPIT_SPEED = 0.35;
     // private static final double SPIT_SPEED = 0.3;
     private static final Current CURRENT_LIMIT = Amps.of(80);
     private static final boolean INVERTED = true;
